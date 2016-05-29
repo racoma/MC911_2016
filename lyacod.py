9e5eca1b30dcd88fb6b87fb92045bc82ada5d325
racoma <raissa.machado.bre@gmail.com>
2016-05-29 14:48:09 -0300

    Started Cod Generating

---
+++ lyacod.py
@@ -0,0 +1,126 @@
+'''
+LVM code is only allowed to use the following operators:
+    (’ldc’, k)     # Load constant sp=sp+1;  M[sp]=k
+    (’ldv’, i, j)  # Load value sp=sp+1;  M[sp]=M[D[i]+j]
+    (’ldr’, i, j)  # Load reference sp=sp+1;  M[sp]=D[i]+j
+    (’stv’, i, j)  # Store value M[D[i]+j]=M[sp];  sp=sp-1
+    (’lrv’, i, j)  # Load reference value sp=sp+1;  M[sp]=M[M[D[i]+j]]
+    (’srv’, i, j)  # Store reference value M[M[D[i]+j]]=M[sp];  sp=sp-1
+    (’add’)        # Add M[sp-1]=M[sp-1] + M[sp];  sp=sp-1
+    (’sub’)        # Subtract M[sp-1]=M[sp-1] - M[sp];  sp=sp-1
+    (’mul’)        # Multiply M[sp-1]=M[sp-1] * M[sp];  sp=sp-1
+    (’div’)        # Division M[sp-1]=M[sp-1] / M[sp];  sp=sp-1
+    (’mod’)        # Modulus M[sp-1]=M[sp-1] % M[sp];  sp=sp-1
+    (’neg’)        # Negate M[sp]= -M[sp]
+    (’and’)        # Logical And M[sp-1]=M[sp-1] and M[sp];  sp=sp-1
+    (’lor’)        # Logical Or M[sp-1]=M[sp-1] or M[sp];  sp=sp-1
+    (’not’)        # Logical Not M[sp]= not M[sp]
+    (’les’)        # Less M[sp-1]=M[sp-1] < M[sp];  sp=sp-1
+    (’leq’)        # Less or Equal M[sp-1]=M[sp-1] <= M[sp];  sp=sp-1
+    (’grt’)        # Greater M[sp-1]=M[sp-1] > M[sp];  sp=sp-1
+    (’gre’)        # Greater or Equal M[sp-1]=M[sp-1] >= M[sp];  sp=sp-1
+    (’equ’)        # Equal M[sp-1]=M[sp-1] == M[sp];  sp=sp-1
+    (’neq’)        # Not Equal M[sp-1]=M[sp-1] != M[sp];  sp=sp-1
+    (’jmp’, p)     # Jump pc=p
+    (’jof’, p)     # Jump on False (if not M[sp]: pc=p) (else: pc=pc+1) (sp=sp-1)
+    (’alc’, n)     # Allocate memory sp=sp+n
+    (’dlc’, n)     # Deallocate memory sp=sp-n
+    (’cfu’, p)     # Call Function sp=sp+1; M[sp]=pc+1; pc=p
+    (’enf’, k)     # Enter Function sp=sp+1; M[sp]=D[k]; D[k]=sp+1
+    (’ret’, k, n)  # Return from Function D[k]=M[sp]; pc=M[sp-1]; sp=sp-(n+2)
+    (’idx’, k)     # Index M[sp-1]=M[sp-1] + M[sp] * k sp=sp-1
+    (’grc’)        # Get(Load) Reference Contents M[sp]=M[M[sp]]
+    (’lmv’, k)     # Load multiple values 
+    (’smv’, k)     # Store multiple Values 
+    (’sts’, k)     # Store string constant on reference 
+    (’rdv’)        # Read single Value sp=sp+1;  M[sp]=input()
+    (’rds’)        # Read String and store it on stack reference 
+    (’prv’)        # Print Value print(M[sp]); sp=sp-1
+    ('prt', k)     # Print Multiple Values print(M[sp-k+1:sp+1]); sp-=(k-1)
+    (’prc’, i)     # Print String constant print(H(i),end="")
+    (’prs’)        # Print contents of a string location 
+    (’stp’)        # Start Program sp=-1; D[0]=0
+    (’lbl’, i)     # No operation (define the label index i)
+    (’end’)        # Stop execution
+
+'''
+
+import sys
+import lyasem
+import lyaparser
+import lyablock
+from collections import defaultdict
+
+
+class GenerateCode(lyaparser.NodeVisitor):
+
+    def __init__(self):
+        super(GenerateCode, self).__init__()
+
+        self.temp_count = 0
+        
+        ''' The generated code (list of tuples)'''
+        self.code = lyablock.BasicBlock()
+        self.start_block = self.code
+
+        ''' A list of external declarations (and types)'''
+        self.externs = []
+        
+    def new_temp(self,tipo):
+        #Creates new temporary variable of a given type
+        name = "__%s_%d" % (tipo,self.temp_count)
+        self.temp_count += 1
+        return name
+
+    def visit_Program(self,node):
+        # Reset the sequence of instructions and temporary count
+        self.code = lyablock.BasicBlock()
+        self.program = self.code
+        self.temp_count = 0
+        inst = ('stp')
+        self.code.append(inst)
+        # Visit all of the statements in the program
+        self.visit(node.statement_list)
+        inst2 = ('end')
+        self.code.append(inst2)
+
+    def visit_Constant(self,node):
+        inst = ('ldc', node.exp)
+        self.code.append(inst)
+
+    def visit_ID(self, node):
+        target = self.new_temp(node.type)
+        inst = ('ldv', node.char, target)
+        self.code.append(inst)
+        node.gen_location = target
+
+    def visit_Syn(self, node):   
+        self.visit(node.expr)
+
+    def visit_Binop(self,node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+        # Create the opcode and append to list
+        opcode = node.type.bin_opc[node.op]
+        inst = (opcode)
+        self.code.append(inst)
+
+    def visit_Operand(self,node):
+        self.visit(node.expr)
+        target = self.new_temp(node.type)
+        opcode = node.type.un_opc[node.op]
+        inst = (opcode)
+        inst2 = ('ldv', node.ident, target)
+        self.code.append(inst2)
+        self.code.append(inst)
+        node.gen_location = target
+
+def gen_code(node):
+    #adicionar teste sem erros lyasem
+    gen = GenerateCode()
+    gen.visit(node)
+    return gen.code
+
+code = gen_code(lyaparser.p)
+
